463c1fd4b2c4a31739cb1e6b8cdf7f89239fd6ff
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-04-10 12:18:22 -0500

    add Material-UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,
-  Route } from 'react-router-dom'; 
+  Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Home from './components/Home';
 import Detail from './components/Detail';
 import Form from './components/Form';
@@ -10,12 +11,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <MuiThemeProvider>
           <Route exact path="/" component={Home} />
           <Route path="/restaurant" component={Detail} />
           <Route path="/reserve" component={Form} />
           <Route path="/confirm" component={Confirmation} />
-        </div>
+        </MuiThemeProvider>
       </Router>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const MainComponent = () => {
     return(
+        
         <div>
             <h1>Title</h1>
             <p>Subtitle</p>
-            <img src="https://dummyimage.com/1200x500/000/fff" alt="dummy-image" />
+            <img src="img/banner_home.png" alt="dummy-image" />
         </div>
     )
 };
